e8a847d521bad5d40021c6978d281e9461e20c78
Murillo Pinheiro de Oliveria <83728277+MuriWolf@users.noreply.github.com>
2022-11-01 22:26:00 -0300

    api update

--- script.js
+++ script.js
@@ -1,12 +1,62 @@
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '54a8813819msh2b80e70b290d77ep10fcfdjsn538c63a37ef4',
-// 		'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
-// 	}
-// };
+const wordToSearch = document.querySelector("#word");
+const searchedWord = document.querySelector("#searched-word");
+const errorMessage = document.querySelector("#error-message")
+const phoneticText = document.querySelector("#phonetic-text")
+const phoneticAudio = document.querySelector("#phonetic-audio")
+const definitions = document.querySelector("#definitions")
+const wordInfo = document.querySelector(".word-info")
+const searchWordBtn = document.querySelector(".search-word-btn")
 
-fetch('https://api.dictionaryapi.dev/api/v2/entries/en/rice')
+searchWordBtn.addEventListener("click", () => {
+	searchWord(wordToSearch.value); 
+})
+
+async function searchWord(word) {
+	fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
 	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	.then(response => {
+		handleError(false)
+		console.log(response)
+		searchedWord.textContent = response[0].word 
+		phoneticText.textContent = response[0].phonetics[0].text
+
+		response[0].phonetics.forEach((element, i) => {
+			if(element.audio != "") {
+				phoneticAudio.src = element.audio
+			}
+		});
+		while (definitions.hasChildNodes()) {
+			definitions.removeChild(definitions.firstChild);
+		}
+		response[0].meanings[0].definitions.forEach(element => {
+			let newEl = createEl(definitions, "li")
+			console.log(definitions);
+			newEl.textContent = element.definition + ` (${element.example})`
+			// console.log(element);
+		});
+	})
+	// .catch(err => {
+	// 	handleError(true)
+	// 	errorMessage.textContent = "Word not finded! Try type again.";
+	// });
+}
+
+// let newEl = createEl(wordInfo, "li")
+// newEl.textContent = "aijdiuas"
+
+function handleError(bool) {
+	if (bool === true) {
+		errorMessage.classList.remove("d-none")
+		wordInfo.classList.add("d-none")
+	} else {
+		errorMessage.classList.add("d-none")
+		wordInfo.classList.remove("d-none")
+	}
+}
+ 
+function createEl(father, el) {
+	console.log(father, el);
+	let newEl = document.createElement(el)
+	father.appendChild(newEl)
+	return newEl
+}
